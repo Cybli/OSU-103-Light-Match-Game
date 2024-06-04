97dcccf009fe9edd4481f8094423dce06644729e
Cybli <peroutkb@oregonstate.edu>
2024-06-04 12:05:26 -0700

    Semi-Final Game
    Polished code, waiting to add difficulty scaling

--- Interactive_Game.ino
+++ Interactive_Game.ino
@@ -1,4 +1,5 @@
 #include <Adafruit_CircuitPlayground.h>
+#include <time.h>
 
 int Score = 0;
 int HP = 2;
@@ -9,10 +10,14 @@ int SwitchPin = 7;
 int UserColor;
 int DeviceColor;
 int DiffScale = 1;
+int r;
+int g;
+int b;
+int RandNum;
 bool Starting = true;
 bool SwitchState;
 volatile bool Lbuttonflag = false;
-volatile bool Rbuttonflag = false;
+volatile bool Rbuttonflag = true;
 volatile bool switchFlag = false;
 void LbuttonISR();
 void RbuttonISR();
@@ -24,10 +29,11 @@ void setup() {
   pinMode(LButtonPin, INPUT_PULLDOWN);
   pinMode(RButtonPin, INPUT_PULLDOWN);
   Serial.begin(9600);
-  !while(Serial)
+  while(!Serial);
   attachInterrupt(digitalPinToInterrupt(LButtonPin), LbuttonISR, FALLING);
   attachInterrupt(digitalPinToInterrupt(RButtonPin), RbuttonISR, FALLING);
   attachInterrupt(digitalPinToInterrupt(SwitchPin), switchISR, CHANGE);
+  srandom(time(0));
 }
 
 void switchISR(){
@@ -48,38 +54,19 @@ void RbuttonISR(){
 
 }
 
-void loop() {
-// HP LOGIC //
-  if(HP = 2){
-     CircuitPlayground.setPixelColor(10, 255, 0, 0);
-     CircuitPlayground.setPixelColor(9, 255, 0, 0);
-  }
-  else if(HP = 1){
-    CircuitPlayground.setPixelColor(10, 255, 0, 0);
-    CircuitPlayground.setPixelColor(9, 0, 0, 0);
-  }
-  else if(HP = 0){
-    CircuitPlayground.setPixelColor(10, 0, 0, 0);
-    CircuitPlayground.setPixelColor(9, 0, 0, 0);
-    Serial.println(Score);
-    //PLay game over sound
-    Starting = true;
-  }
-
-  CircuitPlayground.setPixelColor(8, 255, 255, 255);
-
 // INPUT LOGIC //
+void buttoncheck(){
 if(Rbuttonflag){
   delay(5);
-  CircuitPlayground.setPixelColor(4, 0, 255, 0);
-  Color = 1
+  CircuitPlayground.setPixelColor(2, 0, 255, 0);
+  UserColor = 1;
   Rbuttonflag = false;
 }
 
 if(Lbuttonflag){
   delay(5);
-  CircuitPlayground.setPixelColor(4, 0, 0, 255);
-  Color = 2
+  CircuitPlayground.setPixelColor(2, 0, 0, 255);
+  UserColor = 2;
   Lbuttonflag = false;
 }
 
@@ -89,66 +76,122 @@ if(switchFlag){
   switchFlag = false;
 
 }
+if(SwitchState && (CircuitPlayground.soundSensor() > 70)){
+   CircuitPlayground.setPixelColor(2, 255, 0, 0);
+   UserColor = 3;
+}
+}
+
+// HP LOGIC //
+void HPcheck(){
+if(HP == 2){
+     CircuitPlayground.setPixelColor(9, 255, 0, 0);
+     CircuitPlayground.setPixelColor(8, 255, 0, 0);
+  }
+  else if(HP == 1){
+    CircuitPlayground.setPixelColor(9, 255, 0, 0);
+    CircuitPlayground.setPixelColor(8, 0, 0, 0);
+  }
+  else if(HP == 0){
+    CircuitPlayground.setPixelColor(9, 0, 0, 0);
+    CircuitPlayground.setPixelColor(8, 0, 0, 0);
+  } else if(HP = -1){
+    for(int i = 3; i>= 0; i--){
+    for(int i=10; i>=0; i--) {
+    CircuitPlayground.setPixelColor(i, 255, 0, 0);
+  }
+    Serial.println("Score: ");
+    Serial.print(Score);
+  delay(500);
+  CircuitPlayground.clearPixels();
+   delay(500);
+    }
+    //PLay game over sound
+    HP = 2;
+    Rbuttonflag = true;
+    Starting = true;
+  }
+}
+
+void loop() {
+
+HPcheck();
+
+  CircuitPlayground.setPixelColor(7, 255, 255, 255);
+
+buttoncheck();
 
 //Sound Logic
 
 // GAME LOGIC //
 if(Starting = true){
-  if((Score % (5 * DiffScale)) = 1){
-    DiffScale++;
-  }
+  HPcheck();
 //Random Num
-if(SwitchState){
-//Rand 1-2
+if(!SwitchState){
+ RandNum = random(1, 3);
 }
-else if(!SwitchState){
-//Rand 1-3
+else if(SwitchState){
+ RandNum = random(1, 4);
 }
-if(RandNum = 1){
+if(RandNum == 1){
   r = 0;
   g = 255;
   b = 0;
+  DeviceColor = 1;
 }
-else if(RandNum = 2){
+else if(RandNum == 2){
   r = 0;
   g = 0;
   b = 255;
+  DeviceColor = 2;
 }
-else if(RandNum = 3){
+else if(RandNum == 3){
   r = 255;
   g = 0;
   b = 0;
+  DeviceColor = 3;
 }
 Starting = false;
 }
 
 // Light Logic //
-for(int i=7; i<4; i--) {
+CircuitPlayground.setPixelColor(0, 0, 0, 0);
+Pos = 6;
+for(int i=6; i>2; i--) {
+  buttoncheck();
     CircuitPlayground.setPixelColor(i, r, g, b);
-    if(i != 7){
-    CircuitPlayground.setPixelColor(i++, 0, 0, 0);
+    if(i != 6){
+    CircuitPlayground.setPixelColor(i + 1, 0, 0, 0);
     }
-    delay(250 % DiffScale);
+   // delay(250 % (DiffScale/2);
+   delay(300);
+
     Pos--;
   }
-if(Pos = 4){
-  CircuitPlayground.setPixelColor(5, 0, 0, 0);
-  if(UserColor = DeviceColor){
-  Score = Score++
+if(Pos == 2){
+  CircuitPlayground.setPixelColor(3, 0, 0, 0);
+  if(UserColor == DeviceColor){
+      Score == Score++;
   //Happy Sound
   Starting = true;
+  
   } else {
-    HP = HP--;
-    for(int i=4; i>0; i--) {
+    HP = HP - 1;
+    delay(300);
+    for(int i=1; i>=0; i--) {
+    buttoncheck();
     CircuitPlayground.setPixelColor(i, r, g, b);
-    CircuitPlayground.setPixelColor(i++, 0, 0, 0);
-    delay(250 % DiffScale);
+    if(i != 1){
+    CircuitPlayground.setPixelColor(i + 1, 0, 0, 0);
     }
-    if(DiffScale != 1){
-      DiffScale--;
+    //delay(250 % DiffScale);
+    delay(300);
     }
     Starting = true;
+    
+ 
   }
 
 
+}
 }
